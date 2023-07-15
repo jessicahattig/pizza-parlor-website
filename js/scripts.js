5828060bc5c5455197155caca8bfbc26e0976647
jessicahattig <jessicahattig@gmail.com>
2023-07-15 13:34:10 -0700

    WIP:Form.

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,18 @@ Order.prototype.assignId = function() {
   return this.currentId;
 }
 
-Order.prototype.calculatePrice = function(pizza) {
-  let sizeCost = {
-    small: 8,
-    medium: 10,
-    large: 12,
-  };
-  let toppingsCost = {
-    anchovies: 1,
-    pepperoni: 1,
-    pineapple: 1.5,
-    spinach: 0.5
-  };
+// Order.prototype.calculatePrice = function(pizza) {
+//   let sizeCost = {
+//     small: 8,
+//     medium: 10,
+//     large: 12,
+//   };
+//   let toppingsCost = {
+//     anchovies: 1,
+//     pepperoni: 1,
+//     pineapple: 1.5,
+//     spinach: 0.5
+//   };
 
   let totalPrice = (sizeCost[pizza.size] || 0);
   pizza.toppings.forEach(function(topping) { 
@@ -42,3 +42,19 @@ function Pizza(toppings, size) {
 }
 
 //UI Logic
+window.addEventListener("load", function() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", displayOrder)
+});
+
+function displayOrder(event){
+  event.preventDefault();
+  const selectedItems = [];
+
+  const pizzaSize = document.getElementsByName("size");
+  const selectedSize = Array.from(pizzaSize).find(size => size.checked);
+  const sizeValue = selextedSize ? selectedSize.value : "";
+
+const toppings = document.getElementsByName("toppings");
+const selectedToppings = Array.from(toppings).filters(topping => topping.checked).map(topping => topping.value);
+}
